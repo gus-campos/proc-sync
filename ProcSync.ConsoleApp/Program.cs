99173bcca7dd5f1121b5dde4381b429cc9abe00e
Gustavo Campos <gus.campos042@gmail.com>
2026-04-21 16:09:24 -0300

    muda ordem de comentarios

--- ProcSync.ConsoleApp/Program.cs
+++ ProcSync.ConsoleApp/Program.cs
@@ -6,28 +6,28 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        // Simple counter - Increment and Decrement
+        // Simple counter - Increment
         {
             var counter = new SimpleCounter();
             ParallerCountingTester.RunIncrement(1000, counter);
             Console.WriteLine($"Simple     add e sub: {counter.Count}");
         }
 
-        // Simple counter - Increment
+        // Simple counter - Increment and Decrement
         {
             var counter = new SimpleCounter();
             ParallerCountingTester.RunIncrementAndDecrement(1000, counter);
             Console.WriteLine($"Simple     add e sub: {counter.Count}");
         }
 
-        // Concurrent counter - Increment and Decrement
+        // Concurrent counter - Increment
         {
             var counter = new ConcurrentCounter();
             ParallerCountingTester.RunIncrement(1000, counter);
             Console.WriteLine($"Concurrent add e sub: {counter.Count}");
         }
 
-        // Concurrent counter - Increment
+        // Concurrent counter - Increment and Decrement
         {
             var counter = new ConcurrentCounter();
             ParallerCountingTester.RunIncrementAndDecrement(1000, counter);
